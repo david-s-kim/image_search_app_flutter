d2aa424fcddb2ae73fa7b1adfc0aeb3d8266979e
David Kim <mycodingtool9@gmail.com>
2024-04-11 16:08:46 +0900

    - feat: photo_mapper 생성

---
+++ lib/data/mapper/photo_mapper.dart
@@ -0,0 +1,12 @@
+import 'package:image_search_app_flutter/data/dto/photo_result_dto.dart';
+import 'package:image_search_app_flutter/data/model/photo.dart';
+
+extension ToPhoto on Hit {
+  Photo toPhoto() {
+    return Photo(
+        id: id!.toInt(),
+        url: previewUrl ?? '',
+        tags: tags ?? '',
+    );
+  }
+}
