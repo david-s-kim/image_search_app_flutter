f9894f7777512b8ed52cf7b6bbf199c2a979583b
David Kim <mycodingtool9@gmail.com>
2024-04-11 15:55:38 +0900

    - feat: Photo model 생성

---
+++ lib/data/model/photo.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'photo.freezed.dart';
+part 'photo.g.dart';
+
+@freezed
+class Photo with _$Photo {
+  const factory Photo({
+    required int id,
+    required String url,
+    required String tags,
+  }) = _Photo;
+
+
+
+  factory Photo.fromJson(Map<String, dynamic> json) =>
+      _$PhotoFromJson(json);
+}
