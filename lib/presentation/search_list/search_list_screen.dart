3a1d787de19d5d1761951ca5c0de67697595df6b
David Kim <mycodingtool9@gmail.com>
2024-04-12 14:29:19 +0900

    - refactor: search_list_screen, search_list_view_model 코드 수정

--- lib/presentation/search_list/search_list_screen.dart
+++ lib/presentation/search_list/search_list_screen.dart
@@ -1,10 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:image_search_app_flutter/presentation/search_list/search_list_view_model.dart';
+import 'package:provider/provider.dart';
 
-class SearchListScreen extends StatelessWidget {
+import 'components/image_card_widget.dart';
+
+class SearchListScreen extends StatefulWidget {
   const SearchListScreen({super.key});
 
   @override
+  State<SearchListScreen> createState() => _SearchListScreenState();
+}
+
+class _SearchListScreenState extends State<SearchListScreen> {
+  final _queryTextEditingController = TextEditingController();
+
+  @override
+  void dispose() {
+    _queryTextEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final viewModel = context.watch<SearchListViewModel>();
+    final state = viewModel.state;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('이미지 검색하기'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _queryTextEditingController,
+              decoration: InputDecoration(
+                border: const OutlineInputBorder(),
+                hintText: '검색',
+                suffixIcon: IconButton(
+                  icon: const Icon(Icons.search),
+                  onPressed: () {
+                    final query = _queryTextEditingController.text;
+                    viewModel.onSearch(query);
+                  },
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: state.isLoading
+                  ? const Center(
+                      child: CircularProgressIndicator(),)
+                  : GridView.count(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 10,
+                      children: state.photos
+                          .map((e) => ImageCardWidget(photo: e))
+                          .toList(),
+                    ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/presentation/search_list/search_list_view_model.dart
+++ lib/presentation/search_list/search_list_view_model.dart
@@ -1,29 +1,28 @@
 import 'package:flutter/cupertino.dart';
 import 'package:image_search_app_flutter/data/data_source/photo_data_source.dart';
 import 'package:image_search_app_flutter/data/repository/photo_repository.dart';
+import 'package:image_search_app_flutter/presentation/search_list/search_list_state.dart';
 
 import '../../data/model/photo.dart';
 
 class SearchListViewModel with ChangeNotifier {
   final PhotoRepository _photoRepository;
 
-  List<Photo> _photos = [];
+  SearchListViewModel({required PhotoRepository photoRepository})
+      : _photoRepository = photoRepository;
 
-  SearchListViewModel({required PhotoRepository photoRepository}) : _photoRepository = photoRepository;
+  SearchListState _state = const SearchListState();
 
-  List<Photo> get photos => List.unmodifiable(_photos);
-
-  bool _isLoading = false;
-
-  bool get isLoading => _isLoading;
+  SearchListState get state => _state;
 
   void onSearch(String query) async {
-    _isLoading = true;
+    _state = state.copyWith(isLoading: true);
     notifyListeners();
 
-    _photos = await _photoRepository.getPhotos(query);
-    _isLoading = false;
+    _state = state.copyWith(
+      photos: await _photoRepository.getPhotos(query),
+      isLoading: false,
+    );
     notifyListeners();
   }
-
 }
