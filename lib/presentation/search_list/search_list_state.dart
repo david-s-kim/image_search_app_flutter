b7a48b0dbb8314083d2fc5f808c37c4e985fa648
David Kim <mycodingtool9@gmail.com>
2024-04-12 14:28:25 +0900

    - refactor: search_list_state 추가

---
+++ lib/presentation/search_list/search_list_state.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../../data/model/photo.dart';
+
+part 'search_list_state.freezed.dart';
+part 'search_list_state.g.dart';
+
+@freezed
+class SearchListState with _$SearchListState {
+  const factory SearchListState({
+    @Default([]) List<Photo> photos,
+    @Default(false) bool isLoading,
+  }) = _SearchListState;
+
+
+
+  factory SearchListState.fromJson(Map<String, dynamic> json) =>
+      _$SearchListStateFromJson(json);
+}
