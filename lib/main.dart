b6c889683b8894e36c12ba741a81f1dad514fb75
David Kim <mycodingtool9@gmail.com>
2024-04-22 14:54:50 +0900

    - feat: Main에서 router 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:image_search_app_flutter/core/router.dart';
 import 'package:image_search_app_flutter/data/data_source/photo_data_source.dart';
 import 'package:image_search_app_flutter/data/repository/photo_repository_impl.dart';
+import 'package:image_search_app_flutter/di/di_setup.dart';
 import 'package:image_search_app_flutter/domain/model/photo.dart';
 import 'package:image_search_app_flutter/domain/use_case/get_photos_use_case.dart';
 import 'package:image_search_app_flutter/presentation/search_list/search_list_view_model.dart';
@@ -8,7 +10,8 @@ import 'package:provider/provider.dart';
 
 import 'presentation/search_list/search_list_screen.dart';
 
-void main() {
+void main() async {
+  await diSetup();
   runApp(const MyApp());
 }
 
@@ -18,21 +21,29 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    // return MaterialApp(
+    //   title: 'Flutter Demo',
+    //   theme: ThemeData(
+    //     colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    //     useMaterial3: true,
+    //   ),
+    //   home: ChangeNotifierProvider(
+    //     create: (_) => SearchListViewModel(
+    //       getPhotosUseCase: GetPhotosUseCase(
+    //         PhotoRepositoryImpl(
+    //           photoDataSource: PhotoDataSource(),
+    //         ),
+    //       ),
+    //     ),
+    //     child: const SearchListScreen(),
+    //   ),
+    // );
+    return MaterialApp.router(
+      routerConfig: router,
+      title: 'Image Search',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrangeAccent),
         useMaterial3: true,
-      ),
-      home: ChangeNotifierProvider(
-        create: (_) => SearchListViewModel(
-          getPhotosUseCase: GetPhotosUseCase(
-            PhotoRepositoryImpl(
-              photoDataSource: PhotoDataSource(),
-            ),
-          ),
-        ),
-        child: const SearchListScreen(),
       ),
     );
   }
