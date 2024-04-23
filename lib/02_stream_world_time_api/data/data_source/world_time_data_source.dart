1bd8969c67c87499633440696a641be77aaf853d
David Kim <mycodingtool9@gmail.com>
2024-04-23 16:45:33 +0900

    - fix: world_time_data_source.dart build_runner

--- lib/02_stream_world_time_api/data/data_source/world_time_data_source.dart
+++ lib/02_stream_world_time_api/data/data_source/world_time_data_source.dart
@@ -1,7 +1,10 @@
 import 'package:dio/dio.dart';
 import 'package:retrofit/http.dart';
+import 'package:json_annotation/json_annotation.dart';
 
 import '../dto/time_dto.dart';
+
+part 'world_time_data_source.g.dart';
 
 @RestApi(baseUrl: 'https://worldtimeapi.org/api/')
 abstract class WorldTimeDataSource {
